19de5735fdb8f38ca2b67cfb284059ae7bfd151a
Akinwande Oladipo <54485470+Akinwande-Mhayormarkdee-Oladipo@users.noreply.github.com>
2024-07-30 10:00:47 +0100

    Update

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 
-
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title("My Snake Game")
 screen.tracer(0)
-
 
 snake = Snake()
 food = Food()
@@ -21,11 +19,6 @@ screen.onkey(snake.up, 'Up')
 screen.onkey(snake.down, 'Down')
 screen.onkey(snake.left, 'Left')
 screen.onkey(snake.right, 'Right')
-
-
-
-
-
 
 game_is_on = True
 # direction = 'right'
@@ -60,18 +53,5 @@ while game_is_on:
             # snake.reset()
             # food.reset()
             score.game_over()
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 screen.exitonclick()
